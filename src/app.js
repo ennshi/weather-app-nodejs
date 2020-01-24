388fd1ec0f36718abaced816f5e36976c763e97c
ennshi <enn.shi@gmail.com>
2020-01-24 22:16:40 +0100

    port fixed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const forecast = require('./utils/forecast');
 const citiesFinder = require('./utils/allthecities');
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
